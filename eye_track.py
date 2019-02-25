de2109a09be1e3fdcded1c613d156ba1e87e5c53
Subhaditya <msubhaditya@gmail.com>
2019-02-25 18:26:05 +0530

    Readme Update

--- eye_track.py
+++ eye_track.py
@@ -1,74 +1,119 @@
 import numpy as np
 import cv2
 import time
+import autopy
+import seaborn as sns
+import matplotlib.pyplot as plt
+ESCAPE_KEY = 27
+# Standards: use constants like this to follow DRY
+# and reduce the occurrence of "magic numbers" in your code that lack context
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
 
 
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-eye_glass_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-left_counter=0  
-right_counter=0 
-    
-th_value=5   
+def update_mouse_position(hough_circles, eye_x_pos, eye_y_pos, roi_color2):
+    try:
+        for circle in hough_circles[0, :]:
+            # Standards: DRY (don't repeat yourself), define circle_center once and use it twice.
+            circle_center = (circle[0], circle[1])
+            # draw the outer circle
+            cv2.circle(
+                img=roi_color2,
+                center=circle_center,
+                radius=circle[2],
+                color=WHITE,
+                thickness=2
+            )
+            # print("drawing circle")
+            # draw the center of the circle
+            cv2.circle(
+                img=roi_color2,
+                center=circle_center,
+                radius=2,
+                color=WHITE,
+                thickness=3
+            )
 
-def thresholding( value ):
-    global left_counter
-    global right_counter
-    
-    if (value<=54):   
-        left_counter=left_counter+1
+            # print(i[0],i[1])
 
-        if (left_counter>th_value):
-            print('RIGHT')  
-            left_counter=0  
+            x_pos = int(eye_x_pos)
+            y_pos = int(eye_y_pos)
+            autopy.mouse.move(x_pos, y_pos)
+    except Exception as e:
+        # Standards: exception handling in try: except cases should generally be as specific as possible. What type of
+        # exception are you expecting to encounter here? Instead of capturing "Exception" capture that specific class of exception.
+        print('Exception:', e)
 
-    elif(value>=54):  
-        right_counter=right_counter+1
+face_cascade = cv2.CascadeClassifier(
+    'haarcascades/haarcascade_frontalface_default.xml'
+)
+eye_cascade = cv2.CascadeClassifier(
+    'haarcascades/right_eye_2_split.xml'
+)
 
-        if(right_counter>th_value):
-            print('LEFT')
-            right_counter=0
+def run_tracker():
+    #number signifies camera
+    video_capture = cv2.VideoCapture(0)
+    time.sleep(2)
+    eye_x_positions = list()
+    eye_y_positions = list()
 
-def main(inp):
-    cap = cv2.VideoCapture(0)
-    p =0
-    bex,bey,bew,beh=0,0,0,0
-    while 1:
-        ret, img = cap.read()
-        img = cv2.flip(img, 1)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        
-        for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-            eyes = inp.detectMultiScale(roi_gray)
-            if(len(eyes)!=0 and p%30==0):
-                for (ex,ey,ew,eh) in eyes:
-                    if(ex!=0):
-                        bex,bey,bew,beh = ex,ey,ew,eh
-                        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-                        cv2.imwrite("eyes/eye_{}.jpg".format(p),roi_color[ey:ey+eh, ex:ex+ew])
-            else:
-                print(bex,bey,bew,beh)
-                cv2.rectangle(roi_color,(bex,bey),(bex+bew,bey+beh),(0,255,0),2)
-                cv2.imwrite("eyes/eyen_{}.jpg".format(p),roi_color[bey:bey+beh, bex:bex+bew])
-            p+=1
+    key_pressed = None
 
-        cv2.imshow('img',img)
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
+    while not key_pressed == ESCAPE_KEY:
+        success, image = video_capture.read()
+        image = cv2.flip(image,1)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        eyes = eye_cascade.detectMultiScale(gray)
 
-    cap.release()
+        for (eye_x, eye_y, eye_width, eye_height) in eyes:
+            # Standards: may be good to explicitly call out the parameters being passed to methods,
+            # if you are not explicitly declaring these values as variables with informative names
+            cv2.rectangle(
+                img=image,
+                pt1=(eye_x, eye_y),
+                pt2=(eye_x + eye_width, eye_y + eye_height),
+                color=GREEN,
+                thickness=2
+            )
+            roi_gray2 = gray[eye_y: eye_y + eye_height, eye_x: eye_x + eye_width]
+            roi_color2 = image[eye_y: eye_y + eye_height, eye_x: eye_x + eye_width]
+            # Standards: As above, it may be good to describe the parameters being passed
+            # but in this case I had a hard time reconciling the positional arguments here with expected parameters
+            hough_circles = cv2.HoughCircles(
+                roi_gray2,
+                cv2.HOUGH_GRADIENT,
+                1,
+                200,
+                param1=200,
+                param2=1,
+                minRadius=0,
+                maxRadius=0
+            )
+            # Standards: DRY (don't repeat yourself), calculate eye positions once and use them below
+            eye_x_pos = (eye_x + eye_width) / 2
+            eye_y_pos = (eye_y + eye_height) / 2
+            print(eye_x_pos, eye_y_pos)
+            eye_x_positions.append(eye_x_pos)
+            eye_y_positions.append(eye_y_pos)
+
+            # Standards: in general in the interests of improving readability, move logical chunks
+            # of code out into their own classes, methods, or functions to make it easier to understand overall
+            # program flow
+            update_mouse_position(hough_circles, eye_x_pos*2, eye_y_pos*2, roi_color2)
+
+        cv2.imshow('img', image)
+        cv2.moveWindow("img", 100,100);
+
+        # Standards: code like this can be hard to understand, so comments explicitly describing operation are desirable:
+        # This reduces cv2.waitKey() response to 8 bit integer, representing ASCII input
+        key_pressed = cv2.waitKey(30) & 0xff
+
+    # close window
+    video_capture.release()
     cv2.destroyAllWindows()
 
-def choice():
-    n = int(input('1 if you wear glasses, 2 if you dont '))
-    if n ==1:
-        main(eye_glass_cascade)
-    else:
-        main(eye_cascade)
-choice()
-1121
+
+if __name__ == "__main__":
+    run_tracker()
